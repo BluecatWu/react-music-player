835e3982f929fe28daa4d062bda6a5a6b3628706
wuxiaodong <wuxiaodong@zhuku.co>
2017-10-17 13:09:56 +0800

    播放,上一首,下一首
    更新ui

---
+++ app/player/player.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import Progress from '../components/progress'
+import  './Player.less'
+import  {MUSIC_LIST}  from './../config/musiclist';
+import { Link } from 'react-router-dom'
+import Pubsub from 'pubsub-js'
+
+/*总时间*/
+let duration = null;
+class Player extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('player',this)
+        this.state = {
+            progress: 0,
+            volume: 0,
+            time:0,
+            currentMusicItem: MUSIC_LIST[0],
+            isPlay:true
+        };
+    }
+    componentDidMount(){
+        $('#player').bind($.jPlayer.event.timeupdate, (e)=>{
+            /*总时间*/
+            duration=e.jPlayer.status.duration;
+            this.setState({
+                volume : e.jPlayer.options.volume * 100,
+                time :e.jPlayer.status.currentTime,
+                progress: e.jPlayer.status.currentPercentAbsolute,
+            })
+        });
+    }
+    componentWillUnmount(){
+        $('#player').unbind($.jPlayer.event.timeupdate);
+    }
+    progressChangeHandler(progress){
+        $('#player').jPlayer(this.state.isPlay?'play':'pause',duration * progress);
+    }
+    changeVolumeHanler(progress){
+        $('#player').jPlayer('volume',progress)
+    }
+    play(){
+        this.state.isPlay ? $('#player').jPlayer('pause') : $('#player').jPlayer('play');
+        this.setState({
+            isPlay:!this.state.isPlay
+        })
+    }
+	playPrev(){
+    		Pubsub.publish('PLAY_PREV');
+            this.setState({
+                isPlay:true
+            })
+    	}
+	playNext(){
+    		Pubsub.publish('PLAY_NEXT');
+            this.setState({
+                isPlay:true
+            })
+    	}
+    render(){
+        return (
+            <div className='player-page'>
+                <h1 className='caption'><Link to='/list'>我的私人音乐坊</Link> </h1>
+                <div className="mt20 row">
+                    <div className="controll-wrapper">
+                        <h2 className="music-title">{this.state.currentMusicItem.title}</h2>
+                        <h3 className="music-artist mt10">{this.state.currentMusicItem.artist}</h3>
+                        <div className="row mt20">
+                            <div className="volume-container">
+                                <i className="icon-volume rt" style={{top:5,left:-5}}></i>
+                                <div className="volume-wrapper">
+                                    <Progress progress={this.state.volume} barColor="red" onProgressChange={this.changeVolumeHanler} />
+                                </div>
+                            </div>
+                        </div>
+                        <div style={{height:10,lineHeight:'10px'}}>
+                            <Progress progress={this.state.progress} onProgressChange={this.progressChangeHandler.bind(this)} />
+                            <div className="left-time -col-auto">{(this.state.time/60).toFixed(0)>10?10:'0'+(this.state.time/60).toFixed(0)} : {((this.state.time).toFixed(0)%60<10) ? '0'+(this.state.time).toFixed(0)%60 : (this.state.time).toFixed(0)%60} s</div>
+                        </div>
+                        <div className="mt35 row">
+                            <div>
+                                <i className="icon prev" onClick={this.playPrev.bind(this)}></i>
+                                <i className={`icon ml20 ${Math.ceil(this.state.time)>= Math.ceil(duration) ? 'play' : this.state.isPlay ? 'pause':'play'}`} onClick={this.play.bind(this)}></i>
+                                <i className="icon next ml20" onClick={this.playNext.bind(this)}></i>
+                            </div>
+                            <div className="-col-auto">
+                                <i className="icon repeat-cycle"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="-col-auto cover">
+                        <img src={this.state.currentMusicItem.cover} alt={this.state.currentMusicItem.title}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Player;
--- app/root.js
+++ app/root.js
@@ -1,34 +1,105 @@
-import React from 'react'
-import Header from './components/header'
-import Progress from './components/progress'
+import React from 'react';
+import Header from './components/header';
+import Player from './player/player';
+import  {MUSIC_LIST}  from './config/musiclist';
+import Musiclist from './player/musiclist';
+import Pubsub from 'pubsub-js'
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom'
+
 class App extends React.Component{
     constructor(props) {
         super(props);
+        // console.log(this.props)
         this.state = {
-            progress: "-",
-            musiclist : '',
-            currentMusicItem: '',
-            isPlay:null
+            musiclist : MUSIC_LIST,
+            currentMusicItem: MUSIC_LIST[0]
         };
+    }
+    playMusic(musicItem){
+        $('#player').jPlayer('setMedia',{
+            mp3 : musicItem.file
+        }).jPlayer('play');
+        this.setState({
+            currentMusicItem:musicItem
+        })
+    }
+    playNext(type= 'next'){
+        let index = this.findMusicIndex(this.state.currentMusicItem);
+        let newIndex=null;
+        let length=this.state.musiclist.length;
+        if(type==='next'){
+            newIndex=(index+1)%length;
+        }else{
+            newIndex=(index-1+length)%length;
+        }
+        this.playMusic(this.state.musiclist[newIndex])
+    }
+    findMusicIndex(musicItem){
+        return  this.state.musiclist.indexOf(musicItem);
     }
     componentDidMount(){
         $('#player').jPlayer({
             supplied : 'mp3',
             wmode : 'window'
         });
+        this.playMusic(this.state.currentMusicItem);
+        $('#player').bind($.jPlayer.event.ended,(e)=>{
+            this.playNext();
+        })
+
+        Pubsub.subscribe('DELETE_MUSIC' , (msg , musicItem) =>{
+            if(this.state.currentMusicItem === musicItem){
+                this.playNext('next');
+            }
+            this.setState({
+                musiclist : this.state.musiclist.filter(item=>{
+                    return item !== musicItem
+                })
+            });
+        } )
+
+        Pubsub.subscribe('PLAY_MUSIC' , (msg , musicItem) =>{
+            this.playMusic(musicItem)
+        } )
+        Pubsub.subscribe('PLAY_PREV' , (msg ) =>{
+            this.playNext('prev')
+        } )
+        Pubsub.subscribe('PLAY_NEXT' , (msg) =>{
+            this.playNext('next')
+        } )
+    }
+    componentWillUnMount(){
+        Pubsub.unsubscribe('DELETE_MUSIC');
+        Pubsub.unsubscribe('PLAY_MUSIC');
+        Pubsub.unsubscribe('PLAY_PREV');
+        Pubsub.unsubscribe('PLAY_NEXT');
+        $('#player').unbind($.jPlayer.event.ended)
     }
     render(){
+        console.log(this.state);
         return (
             <div>
                 <Header />
-                <Progress
-                    progress="1"
-                >
-                </Progress>
+                <Route path='/' component={Player} />
+                <Route path='/list' component={Musiclist} />
             </div>
         )
     }
-
 }
 
-export default App
+class Root extends React.Component {
+    render(){
+        return (
+            <Router >
+                <Route path='/' component={App}>
+
+                </Route>
+            </Router>
+        )
+    }
+}
+
+export default Root;
